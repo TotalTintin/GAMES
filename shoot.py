f3bbb1417fc92d58fc8929c027b3c1a1377b3f07
TotalTintin <85099306+TotalTintin@users.noreply.github.com>
2022-06-10 09:37:15 +0300

    Add files via upload
    Me trying out, the  Coding games in python book . May be you can too.
    Just run it on IDLE and make sure you have Pygame Zero installed.

---
+++ shoot.py
@@ -0,0 +1,23 @@
+import pgzrun
+from random import randint
+apple = Actor("apple")
+
+def draw():
+    screen.clear()
+    apple.draw()
+
+def place_apple():
+    apple.x = randint(10,800)
+    apple.y = randint(10,600)
+
+def on_mouse_down(pos):
+    if apple.collidepoint(pos):
+        print("good shot!")
+        place_apple()
+    else:
+        print("you missed")
+        quit()
+place_apple()
+
+    
+pgzrun.go()
